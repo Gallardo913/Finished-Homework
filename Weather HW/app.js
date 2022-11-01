8577a5aac59b44523a9d078b439bad883f3d3871
Gallardo913 <113468185+Gallardo913@users.noreply.github.com>
2022-11-01 00:59:35 -0400

    Add files via upload

---
+++ Weather HW/app.js
@@ -0,0 +1,77 @@
+// Constant and variables
+
+const apiKey = '554c622242f711493d8a87f48a3d32d6';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
+
+
+// // function that does weather search
+// function weatherSearch(cityname){
+
+// //     //constructing URL for request
+// //     const url = '${baseURL}?q=${cityname}&appid=${apiKey}'
+
+// //     //make our request
+// //     $.ajax(url)
+// //     .then((coord)) => {
+// //         console.log(coord)
+    
+// //         //render data
+// //         const $main = $("main")
+// //         $main.empty()
+
+// //         $main.html('
+// //         <h1>$(weather.Lon}</h1>
+        
+// //         ')
+// //     })
+// // }
+
+// // //click event on submit button
+// // $("input[type=submit]").on("click",(event) => {
+
+// //     event.preventDefault()
+
+// //     constinputText = $("input[type=text]").val()
+
+// //     weatherSearch(inputText)
+// // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Cached Elements References 
+
+
+
+// //Event Listeners 
+
+
+
+// //Functions
+
+// function handleGetData() {
+//     $.ajax(baseURL + 'q=Dallas&appid=' + apiKey)
+//     .then(function(data){
+//         console.log('Data: ', data);
+//     }, function(error) {
+//         console.log('Error: ', error);
+//     })    
+// }
